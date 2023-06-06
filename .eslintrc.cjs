3999cfdff2bc22087ba57fe9e8aac17b920734bf
Peter Qiu <peter@sztus.com>
2023-06-06 16:42:22 +0800

    init project with eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,36 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  "env": {
+    "browser": true,
+    "es2021": true
   },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended"
+  ],
+  "overrides": [
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "project": './tsconfig.json', // 指定 tsconfig.json 文件的路径
+    "ecmaFeatures": {
+      jsx: true,
+    },
+    "parser": '@typescript-eslint/parser',
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint",
+    "react-hooks"
+  ],
+  "rules": {
+    "@typescript-eslint/no-explicit-any": 'off',
+    "no-debugger": 'off', // 允许断点调试
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "off", // 这个规则的作用是检查 "useEffect" 钩子中依赖项数组是否包含所有需要的变量或函数
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      react(),
+      eslintPlugin({
+        include: ['src/**/*.ts', 'src/**/*.tsx', 'src/*.ts', 'src/*.tsx']
+      })
+    ]
+  }
 })
