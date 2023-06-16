1485a26ad9d1bc454d4d37618a3dc0fb8e51928a
Peter Qiu <peter@sztus.com>
2023-06-16 14:43:36 +0800

    loading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react'
 import Home from './pages/home'
+import LoadingPage from '@/pages/loading-page'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import './global.css'
 
@@ -8,8 +9,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/loading" element={<LoadingPage />}></Route>
         <Route path="/home" element={<Home />}></Route>
-        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/" element={<Navigate to="/loading" />} />
       </Routes>
     </BrowserRouter>
   )
