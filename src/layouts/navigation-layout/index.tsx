47b7b78cbb6957aa032f0c365a701077f2bceacf
Peter Qiu <peter@sztus.com>
2023-06-16 14:17:33 +0800

    navigation layout

--- src/layouts/navigation-layout/index.tsx
+++ src/layouts/navigation-layout/index.tsx
@@ -1,5 +1,13 @@
 import React, { ReactNode, useState, useEffect } from 'react'
 import './index.css'
+import Jarvis from '@/component/jarvis'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faTag } from '@fortawesome/free-solid-svg-icons'
+import { faComment } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { Modal } from 'antd'
 
 //-----------类型定义写外面（如果写在函数里面，那么每次render都会重新定义给值）
 interface NavigationLayoutProps {
@@ -8,7 +16,7 @@ interface NavigationLayoutProps {
 
 // 一般函数组件+ts的规范写法，需要定义 props 类型方便检查
 const NavigationLayout: React.FC<NavigationLayoutProps> = ({ children }) => {
-  const [isTop, setIsTop] = useState(false)
+  const [isTop, setIsTop] = useState(true)
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY
@@ -21,6 +29,25 @@ const NavigationLayout: React.FC<NavigationLayoutProps> = ({ children }) => {
     }
   })
 
+  const navItems = [
+    { title: 'Home', link: '/', icon: faHouse },
+    { title: 'About', link: '/about', icon: faTag },
+    { title: 'Contact', link: '/contact', icon: faComment },
+    {
+      title: '',
+      link: '',
+      icon: faMagnifyingGlass
+    }
+  ]
+
+  const [searchOpen, setSearchOpen] = useState(false)
+
+  const handleSearch = (t) => {
+    if (t === '') {
+      setSearchOpen(true)
+    }
+  }
+
   return (
     <>
       <div className={isTop ? 'bg-transparent topNavigation' : 'topNavigation'}>
@@ -28,11 +55,35 @@ const NavigationLayout: React.FC<NavigationLayoutProps> = ({ children }) => {
           <img src={'../../../public/logo.png'} width={50} height={50} />
           InproQD
         </div>
+        <div className="navigator-wrap">
+          {navItems.map((item, index) => (
+            <Link
+              to={item.link}
+              key={index}
+              onClick={() => {
+                handleSearch(item.title)
+              }}
+            >
+              <div className="navigator-tab px-4">
+                <FontAwesomeIcon icon={item.icon} className="pr-2"></FontAwesomeIcon>
+                <span>{item.title}</span>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
       <div>
         <main className="pb-12">{children}</main>
       </div>
       <div className="footer"></div>
+      <div className="aside-navigator">
+        <Jarvis></Jarvis>
+      </div>
+      <Modal title="Modal 1000px width" centered open={searchOpen} width={1000}>
+        <p>some contents...</p>
+        <p>some contents...</p>
+        <p>some contents...</p>
+      </Modal>
     </>
   )
 }
