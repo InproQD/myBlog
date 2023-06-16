8433493408531e37f7a58b0ee5e0b4d91a07bcb4
Peter Qiu <peter@sztus.com>
2023-06-16 18:34:07 +0800

    loading

--- src/pages/loading-page/index.tsx
+++ src/pages/loading-page/index.tsx
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './index.css'
+
 const LoadingPage = () => {
-  const [loading, setLoading] = useState(true)
-  setInterval(() => {
-    setLoading(false)
+  const Navigate = useNavigate()
+  setTimeout(() => {
+    Navigate('/home')
   }, 2000)
+
   return (
     <>
-      {loading && (
-        <div className="loading-wrap">
-          <div className="spinner">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-          <div className="mt-12 text-white">加载中...</div>
+      <div className="loading-wrap">
+        <div className="spinner">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-      )}
-      {!loading && (
-        <div className="open-wrap">
-          <div className="open-left"></div>
-          <div className="open-right"></div>
-        </div>
-      )}
+        <div className="mt-12 text-white">加载中...</div>
+      </div>
+      {/*{!loading && (*/}
+      {/*  <div className="open-wrap">*/}
+      {/*    <div className="open-left"></div>*/}
+      {/*    <div className="open-right"></div>*/}
+      {/*  </div>*/}
+      {/*)}*/}
     </>
   )
 }
