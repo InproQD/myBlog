a4f264441d9e6fe33ab715353d1d4b95f15d1dfb
Peter Qiu <peter@sztus.com>
2023-06-29 19:02:24 +0800

    loading page in APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,62 @@
 import './App.css'
-import React from 'react'
+import '@/pages/loading-page/index.css'
+import React, { useState } from 'react'
 import Home from './pages/home'
-import LoadingPage from '@/pages/loading-page'
+// import LoadingPage from '@/pages/loading-page'
 import ArticlePage from '@/pages/article'
 import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
 import './global.css'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [activeOpen, setActiveOpen] = useState(true)
+
+  setTimeout(() => {
+    // 切换为双开门效果
+    setActiveOpen(false)
+    // 等待200ms再添加动画，给标签绑定动态属性留一定时间
+    setTimeout(() => {
+      const left = document.getElementById('open-left') as HTMLBRElement
+      const right = document.getElementById('open-right') as HTMLBRElement
+      left.classList.add('active')
+      right.classList.add('active')
+      setTimeout(() => {
+        // 取消整个loading的动画
+        setLoading(false)
+      }, 1500)
+    }, 200)
+  }, 2000)
+
   return (
     <HashRouter>
       <Routes>
-        <Route path="/loading" element={<LoadingPage />}></Route>
+        {/*<Route path="/loading" element={<LoadingPage />}></Route>*/}
         <Route path="/home" element={<Home />}></Route>
         <Route path="/content" element={<ArticlePage />}></Route>
-        <Route path="/" element={<Navigate to="/loading" />} />
+        <Route path="/" element={<Navigate to="/home" />} />
       </Routes>
+
+      <>
+        {loading && activeOpen && (
+          <div className="loading-wrap">
+            <div className="spinner">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="mt-12 text-white">加载中...</div>
+          </div>
+        )}
+        {loading && !activeOpen && (
+          <div className="open-wrap">
+            <div className="open-left" id="open-left"></div>
+            <div className="open-right" id="open-right"></div>
+          </div>
+        )}
+      </>
     </HashRouter>
   )
 }
--- src/pages/loading-page/index.tsx
+++ src/pages/loading-page/index.tsx
@@ -1,32 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './index.css'
 
 const LoadingPage = () => {
+  const [loading, setLoading] = useState(true)
   const Navigate = useNavigate()
   setTimeout(() => {
-    Navigate('/home')
-  }, 2000)
+    setLoading(false)
+    setTimeout(() => {
+      const left = document.getElementById('open-left') as HTMLBRElement
+      const right = document.getElementById('open-right') as HTMLBRElement
+      left.classList.add('active')
+      right.classList.add('active')
+      setTimeout(() => {
+        Navigate('/home')
+      }, 500)
+    }, 200)
+  }, 1000)
 
   return (
     <>
-      <div className="loading-wrap">
-        <div className="spinner">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+      {loading && (
+        <div className="loading-wrap">
+          <div className="spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="mt-12 text-white">加载中...</div>
         </div>
-        <div className="mt-12 text-white">加载中...</div>
-      </div>
-      {/*{!loading && (*/}
-      {/*  <div className="open-wrap">*/}
-      {/*    <div className="open-left"></div>*/}
-      {/*    <div className="open-right"></div>*/}
-      {/*  </div>*/}
-      {/*)}*/}
+      )}
+      {!loading && (
+        <div className="open-wrap">
+          <div className="open-left" id="open-left"></div>
+          <div className="open-right" id="open-right"></div>
+        </div>
+      )}
     </>
   )
 }
