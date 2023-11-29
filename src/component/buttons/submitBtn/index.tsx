2420e74fa3205212aafc84dfac9a276beeb272d3
Peter Qiu <peter@sztus.com>
2023-11-29 16:00:14 +0800

    button组件抛出点击事件

--- src/component/buttons/submitBtn/index.tsx
+++ src/component/buttons/submitBtn/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { MouseEventHandler } from 'react'
 import './index.css'
-const submitBtn = () => {
-  return <button className="submit-btn">Submit</button>
+interface SubmitBtnProps {
+  onClick: MouseEventHandler<HTMLButtonElement>
+}
+const submitBtn: React.FC<SubmitBtnProps> = ({ onClick }) => {
+  return (
+    <button className="submit-btn" onClick={onClick}>
+      Submit
+    </button>
+  )
 }
 export default submitBtn
