754f1e79ea4067dc8c7bbaa27241782b8557798d
Peter Qiu <peter@sztus.com>
2024-01-12 18:20:29 +0800

    评论功能初步完善

---
+++ src/component/comments-editor/index.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react'
+import './index.css'
+import { Input, Select, Button } from 'antd'
+import { request } from '@/util/request'
+import store from '@/redux/store'
+const { TextArea } = Input
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
+const CommentEditor = (props: { parentId: number; parentName: string; type: string; click: any; update: any }) => {
+  const [userName, setUserName] = useState('')
+  const [comment, setComment] = useState('')
+  const [avatarUrl, setAvatarUrl] = useState('http://123.207.40.28/avatar1.png')
+  const editorType = props.type
+
+  const handleDelete = () => {
+    props.click('delete')
+  }
+
+  const handleFormSubmit = () => {
+    request
+      .post(
+        'http://123.207.40.28:8083/api/set-comment',
+        {
+          name: userName,
+          content: comment,
+          avatar: avatarUrl,
+          parentId: props.parentId || 0,
+          respondent: props.parentName || null
+        },
+        (res: any) => {
+          store.dispatch({ type: 'SET_MESSAGE', value: { msg: res.msg, type: 'success' } })
+        },
+        (res: any) => {
+          store.dispatch({ type: 'SET_MESSAGE', value: { msg: res.msg, type: 'error' } })
+        }
+      )
+      .then(props.update())
+  }
+
+  return (
+    <div
+      className="form-section pa-2"
+      style={editorType === 'reply' ? { border: '1px solid grey', borderRadius: 0 } : {}}
+    >
+      <div className="base-info mb-2">
+        <Input
+          placeholder="name"
+          size={'large'}
+          value={userName}
+          style={{ width: '45%' }}
+          onChange={(res) => {
+            setUserName(res.target.value)
+          }}
+        />
+        <Input type="email" placeholder="email" size={'large'} style={{ width: '45%' }} />
+      </div>
+      <div className="d-flex align-center">
+        <div className="ml-2 mr-3">Avatar:</div>
+        <Select
+          placeholder="avatar"
+          defaultValue="lucy"
+          size={'large'}
+          style={{ width: 200 }}
+          options={
+            [
+              {
+                value: 'http://123.207.40.28/avatar1.png',
+                label: 'Lucy'
+              },
+              { value: 'http://123.207.40.28/avatar2.png', label: 'Andy' },
+              {
+                value: 'http://123.207.40.28/avatar3.png',
+                label: 'Tom'
+              },
+              { value: 'http://123.207.40.28/avatar5.png', label: 'Atom' }
+            ] as any
+          }
+          onChange={(res) => {
+            setAvatarUrl(res)
+          }}
+        />
+        <img className="each-comment-avatar ml-3" src={avatarUrl} alt={'avatar'}></img>
+      </div>
+      <TextArea
+        showCount
+        maxLength={100}
+        value={comment}
+        onChange={(res) => {
+          setComment(res.target.value)
+        }}
+        placeholder="input your comment here"
+        style={{ height: 150, resize: 'none' }}
+        className="mt-4"
+      />
+      <div className="mt-6 d-flex justify-space-between align-center">
+        <button className="comment-submit-btn" onClick={handleFormSubmit}>
+          Submit
+        </button>
+        {editorType === 'reply' && (
+          <Button type="primary" danger icon={<FontAwesomeIcon icon={faWindowClose} />} onClick={handleDelete} />
+        )}
+      </div>
+    </div>
+  )
+}
+export default CommentEditor
--- src/pages/comment/index.tsx
+++ src/pages/comment/index.tsx
@@ -3,18 +3,13 @@ import './index.css'
 import NavigationLayout from '@/layouts/navigation-layout/index'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment } from '@fortawesome/free-solid-svg-icons'
-import { Input, Select } from 'antd'
 import { request } from '@/util/request'
 import store from '@/redux/store'
-const { TextArea } = Input
+import CommentEditor from '@/component/comments-editor'
 
 const CommentPage = () => {
   const [comments, setComments] = useState([] as any)
   const [subComments, setSubComments] = useState([] as any)
-  const [userName, setUserName] = useState('')
-  const [comment, setComment] = useState('')
-  const [avatarUrl, setAvatarUrl] = useState('http://123.207.40.28/avatar1.png')
-
   useEffect(() => {
     listComments()
   }, [])
@@ -22,11 +17,14 @@ const CommentPage = () => {
   const listComments = () => {
     request
       .get(
-        'http://127.0.0.1:8083/api/get-comment-list',
+        'http://123.207.40.28:8083/api/get-comment-list',
         null,
         (res: any) => {
-          setComments(res.list.filter((i) => i.parentId === 0))
-          setSubComments([...res.list.filter((i) => i.parentId !== 0)])
+          res.list.forEach((i: any) => {
+            i.editorDisplay = false
+          })
+          setComments(res.list.filter((i: any) => i.parentId === 0))
+          setSubComments([...res.list.filter((i: any) => i.parentId !== 0)])
         },
         (res: any) => {
           store.dispatch({ type: 'SET_MESSAGE', value: { msg: res.msg, type: 'error' } })
@@ -35,22 +33,30 @@ const CommentPage = () => {
       .then()
   }
 
-  const handleFormSubmit = () => {
-    request
-      .post(
-        'http://127.0.0.1:8083/api/set-comment',
-        { name: userName, content: comment, avatar: avatarUrl, parentId: 0 },
-        (res: any) => {
-          store.dispatch({ type: 'SET_MESSAGE', value: { msg: res.msg, type: 'success' } })
-          listComments()
-        },
-        (res: any) => {
-          store.dispatch({ type: 'SET_MESSAGE', value: { msg: res.msg, type: 'error' } })
-        }
+  const handleParentReply = (id: number) => {
+    setComments((preComment: any) =>
+      preComment.map((item: any) =>
+        item.id === id ? { ...item, editorDisplay: true } : { ...item, editorDisplay: false }
       )
-      .then()
+    )
+    setSubComments((preComment: any) => preComment.map((item: any) => ({ ...item, editorDisplay: false })))
+  }
+  const handleSubReply = (id: number) => {
+    setSubComments((preComment: any) =>
+      preComment.map((item: any) =>
+        item.id === id ? { ...item, editorDisplay: true } : { ...item, editorDisplay: false }
+      )
+    )
+    setComments((preComment: any) => preComment.map((item: any) => ({ ...item, editorDisplay: false })))
   }
 
+  const handleSubDelete = (type: string) => {
+    if (type === 'sub') {
+      setSubComments((preComment: any) => preComment.map((item: any) => ({ ...item, editorDisplay: false })))
+    } else {
+      setComments((preComment: any) => preComment.map((item: any) => ({ ...item, editorDisplay: false })))
+    }
+  }
   return (
     <NavigationLayout>
       <div className="top-background"></div>
@@ -71,63 +77,7 @@ const CommentPage = () => {
           <FontAwesomeIcon icon={faComment} className="mr-3"></FontAwesomeIcon>
           <span className="f-w-700">评论区</span>
         </div>
-        <div className="form-section pa-2">
-          <div className="base-info mb-2">
-            <Input
-              placeholder="name"
-              size={'large'}
-              value={userName}
-              style={{ width: '45%' }}
-              onChange={(res) => {
-                setUserName(res.target.value)
-              }}
-            />
-            <Input type="email" placeholder="email" size={'large'} style={{ width: '45%' }} />
-          </div>
-          <div className="d-flex align-center">
-            <div className="ml-2 mr-3">Avatar:</div>
-            <Select
-              placeholder="avatar"
-              defaultValue="lucy"
-              size={'large'}
-              style={{ width: 200 }}
-              options={
-                [
-                  {
-                    value: 'http://123.207.40.28/avatar1.png',
-                    label: 'Lucy'
-                  },
-                  { value: 'http://123.207.40.28/avatar2.png', label: 'Andy' },
-                  {
-                    value: 'http://123.207.40.28/avatar3.png',
-                    label: 'Tom'
-                  },
-                  { value: 'http://123.207.40.28/avatar5.png', label: 'Atom' }
-                ] as any
-              }
-              onChange={(res) => {
-                setAvatarUrl(res)
-              }}
-            />
-            <img className="each-comment-avatar ml-3" src={avatarUrl} alt={'avatar'}></img>
-          </div>
-          <TextArea
-            showCount
-            maxLength={100}
-            value={comment}
-            onChange={(res) => {
-              setComment(res.target.value)
-            }}
-            placeholder="input your comment here"
-            style={{ height: 150, resize: 'none' }}
-            className="mt-4"
-          />
-          <div className="mt-4 d-flex">
-            <button className="comment-submit-btn" onClick={handleFormSubmit}>
-              Submit
-            </button>
-          </div>
-        </div>
+        <CommentEditor type={'comment'} parentId={0} parentName={''} update={listComments} click={0} />
       </div>
 
       <div className="list-comment">
@@ -135,7 +85,7 @@ const CommentPage = () => {
           {comments.length} <span className="f-w-700">评论</span>
         </div>
         <div className="mt-3">
-          {comments.map((item, index) => (
+          {comments.map((item: any, index: number) => (
             <div key={index} className="each-comment-wrap">
               <img className="each-comment-avatar" alt="avatar" src={item.avatar_url} />
               <div className="each-comment-content ml-3">
@@ -144,12 +94,29 @@ const CommentPage = () => {
                 </div>
                 <div className="d-flex justify-space-between">
                   <span className="f-s-12 mt-1">{item.time}</span>
-                  <span className="f-s-14 text-red" style={{ cursor: 'pointer' }}>
+                  <span
+                    className="f-s-14 text-red"
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => {
+                      handleParentReply(item.id)
+                    }}
+                  >
                     回复
                   </span>
                 </div>
                 <div className="py-4">{item.comment_content}</div>
-                {[...subComments.filter((i) => i.parentId === item.id)].map((subItem, subIndex) => (
+                {item.editorDisplay && (
+                  <CommentEditor
+                    type={'reply'}
+                    parentId={item.id}
+                    parentName={item.name}
+                    update={listComments}
+                    click={() => {
+                      handleSubDelete('parent')
+                    }}
+                  />
+                )}
+                {[...subComments.filter((i: any) => i.parentId === item.id)].map((subItem: any, subIndex: number) => (
                   <div key={subIndex} className="each-comment-wrap py-3 pl-2 border-wrap">
                     <img className="each-comment-sub-avatar mt-1" alt="avatar" src={subItem.avatar_url} />
                     <div className="each-comment-sub-content ml-3">
@@ -158,13 +125,30 @@ const CommentPage = () => {
                       </div>
                       <div className="d-flex justify-space-between">
                         <span className="f-s-10 mt-1">{subItem.time}</span>
-                        <span className="f-s-14 text-red" style={{ cursor: 'pointer' }}>
+                        <span
+                          className="f-s-14 text-red"
+                          style={{ cursor: 'pointer' }}
+                          onClick={() => {
+                            handleSubReply(subItem.id)
+                          }}
+                        >
                           回复
                         </span>
                       </div>
                       <div className="py-4">
                         回复 @<span className="text-green">{subItem.respondent}</span>: {subItem.comment_content}
                       </div>
+                      {subItem.editorDisplay && (
+                        <CommentEditor
+                          type={'reply'}
+                          parentId={item.id}
+                          parentName={subItem.name}
+                          update={listComments}
+                          click={() => {
+                            handleSubDelete('sub')
+                          }}
+                        />
+                      )}
                     </div>
                   </div>
                 ))}
